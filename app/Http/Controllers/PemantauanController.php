3d9c0e0bd301dca9cf325baeb0acdf2548e2d173
gedehariyogananda <gedehariyoga@gmail.com>
2024-06-17 10:44:39 +0700

    v1 api

--- app/Http/Controllers/PemantauanController.php
+++ app/Http/Controllers/PemantauanController.php
@@ -38,36 +38,28 @@ class PemantauanController extends Controller
         return view('pemantauan_page.index', compact('initialMarkers'));
     }
 
-    // Fungsi untuk mendapatkan alamat dari koordinat
+
     private function getAddressFromCoordinates($latitude, $longitude)
     {
-        // API endpoint dari OpenStreetMap Nominatim
         $apiUrl = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat={$latitude}&lon={$longitude}";
 
-        // Membuat koneksi cURL
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $apiUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
-        // Eksekusi permintaan cURL
         $response = curl_exec($ch);
         curl_close($ch);
 
-        // Decode respons JSON
         $data = json_decode($response, true);
 
-        // Ambil alamat atau bagian yang sesuai dengan kebutuhan Anda
         $alamat = $data['display_name'] ?? '';
 
         return $alamat;
     }
 
-    // Fungsi untuk mendapatkan URL ikon marker berdasarkan status laporan
     private function getMarkerIconUrl($status_laporan)
     {
-        // Logic untuk menentukan URL ikon berdasarkan status laporan
-        // Misalnya, sesuai dengan logika yang telah Anda tentukan sebelumnya
         switch ($status_laporan) {
             case 'perlu-dukungan':
                 return '/images/pin-kuning.png';
